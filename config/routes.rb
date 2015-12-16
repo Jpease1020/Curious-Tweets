9d3aec2f519f31aa08d518472bb6e7624e027ed4
Justin Pease <justinpease2@gmail.com>
2015-12-16 09:45:05 -0700

    got something on the page from twitter

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get '/auth/twitter', as: :login
   get "/auth/twitter/callback", to: "sessions#create"
   get "/logout", to: "sessions#destroy", as: "logout"
+
+ resources :users, only: [:show]
 end
