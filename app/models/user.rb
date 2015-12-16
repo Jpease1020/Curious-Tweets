6740cded08f57fd1da512853e666c41b5c3d1c22
Justin Pease <justinpease2@gmail.com>
2015-12-15 21:51:51 -0700

    started pulling in rails console to test out

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ActiveRecord::Base
       new_user.oauth_token_secret = auth_info.credentials.secret
     end
   end
+
+  def client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['twitter_key']
+      config.consumer_secret     = ENV['twitter_secret']
+      config.access_token        = oauth_token
+      config.access_token_secret = oauth_token_secret
+    end
+  end
 end
