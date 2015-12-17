86323de884ae30242b0b82ae045165b2b33458ab
Justin Pease <justinpease2@gmail.com>
2015-12-16 19:29:07 -0700

    need to check if this is working

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   get "dashboard", to: "dashboard#index"
   get "profile", to: "users#show", as: "profile"
   resources :tweets, only: [:create, :update, :destroy]
-
+  resources :favorites, only: [:create, :destroy]
 end
