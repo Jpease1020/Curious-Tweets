65b10d4084e513d1deac907b492cbc8964577210
Justin Pease <justinpease2@gmail.com>
2015-12-17 03:11:46 -0700

    I tried

--- test/services/services_test.rb
+++ test/services/services_test.rb
@@ -1,21 +1,18 @@
 require './test/test_helper'
 
 class CuriousTweetsServiceTest < ActiveSupport::TestCase
-  # attr_reader :client
-  #
-  # def setup
-  #   user = User.create
-  #   @client = User.new.client
-  # end
+  attr_reader :client, :user
+  def setup
+    @user = User.create
+    @client = User.new.client
+  end
 
-  # test "#tweet streem" do
-  #   VCR.use_cassette("sunlight_service#tweet streem") do
-  #     tweets = client.
-  #     legislator = tweets.first
-  #
-  #     assert_equal 20, tweet streem.count
-  #     assert_equal "Joni", legislator[:first_name]
-  #     assert_equal "Ernst", legislator[:last_name]
-  #   end
-  # end
+  test "#tweet streem" do
+    VCR.use_cassette("curious_tweets#my_tweets") do
+      tweets = client.user_timeline(user.name)
+      tweet = tweets.first
+
+      assert_equal "Hi", tweet["text"]
+    end
+  end
 end
