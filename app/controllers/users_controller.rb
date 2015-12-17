f67813e9e567e3f1fd8a33e82800abe7c1744d68
Justin Pease <justinpease2@gmail.com>
2015-12-16 23:46:45 -0700

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = current_user.client.user
+    #have the route call the model, the model call the services 
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,23 @@
+require 'simplecov'
+SimpleCov.start
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/pride'
+require 'webmock'
+require 'vcr'
+
+
+
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  VCR.configure do |config|
+    config.cassette_library_dir = "test/cassetts"
+    config.hook_into(:webmock)
+  end
+
 end
