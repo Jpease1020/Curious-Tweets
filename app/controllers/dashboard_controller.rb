2b22f451ef8c9515907d24fb873f747d051f6d0f
Justin Pease <justinpease2@gmail.com>
2015-12-17 13:26:06 -0700

    changes the my tweet user to current user

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   def index
     @user = current_user.client.user
     @tweets = current_user.client.home_timeline
-    @my_tweets = current_user.client.user_timeline("Justin Pease")
+    @my_tweets = current_user.client.user_timeline(@user)
   end
 end
