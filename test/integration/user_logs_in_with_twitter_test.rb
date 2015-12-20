44b34af448b8a57057867042a0d30e291f935eb2
Justin Pease <justinpease2@gmail.com>
2015-12-20 16:41:57 -0700

    should have changed to a testing branch

--- test/integration/user_logs_in_with_twitter_test.rb
+++ test/integration/user_logs_in_with_twitter_test.rb
@@ -7,7 +7,7 @@ class UserLogsInWithTwitterTest < ActionDispatch::IntegrationTest
       visit "/"
       assert_equal 200, page.status_code
       click_link "login"
-      assert_equal "/", current_path
+      assert_equal "/dashboard", current_path
       assert page.has_content?("Justin Pease")
       assert page.has_link?("logout")
     end
