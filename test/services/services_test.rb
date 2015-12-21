a2d955a139f4c76c30a0b976c6030edf9a0b8d9f
Justin Pease <justinpease2@gmail.com>
2015-12-20 17:17:37 -0700

    watching american sniper is causing me to forget to change branches

--- test/services/services_test.rb
+++ test/services/services_test.rb
@@ -3,13 +3,14 @@ require './test/test_helper'
 class CuriousTweetsServiceTest < ActiveSupport::TestCase
   attr_reader :client, :user
   def setup
-    @user = OpenStruct.new(oauth_token: ENV["oauth_token"], oauth_token_secret: ENV["oauth_token_secret"])
-    @client = User.new.client
+    # @user = OpenStruct.new(oauth_token: ENV["oauth_token"], oauth_token_secret: ENV["oauth_token_secret"])
+    # @client = User.new.client
   end
 
 
   test "#tweet streem" do
     VCR.use_cassette("curious_tweets#my_tweets") do
+      byebug
       tweets = client.user_timeline(user.name)
       tweet = tweets.first
 
